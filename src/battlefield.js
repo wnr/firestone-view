b13a87f644dd7c0ff55ce587cf53e6677a540765
Lucas Wiener <lucas@wiener.se>
2016-03-15 21:08:56 +0100

    Fixed with positions a bit more

--- src/battlefield.js
+++ src/battlefield.js
@@ -3,6 +3,26 @@ import ReactDOM from "react-dom";
 
 import Minion from "./minion";
 
+function mix(coll1, coll2) {
+    var bigLength = Math.max(coll1.length, coll2.length);
+
+    var output = [];
+
+    for (var i = 0; i < bigLength; i++) {
+        if (i < coll1.length) {
+            output.push(coll1[i]);
+        }
+
+        if (i < coll2.length) {
+            output.push(coll2[i]);
+        }
+    }
+
+    console.log(output);
+
+    return output;
+}
+
 export default function Battlefield(props) {
     return (
         <div className="battlefield">
@@ -38,28 +58,27 @@ function Side(props) {
             className += " focused";
         }
 
-        var firstPosition;
-        if (props.showPositions) {
-            firstPosition = <li key={minion.position} className="position" onClick={() => props.onPositionClick(position)}></li>;
-        }
-
-        var lastPosition;
-        if (props.showPositions && position === props.minions.length - 1) {
-            var lastPosition = <li key={position + 1} className="position" onClick={() => props.onPositionClick(position + 1)}></li>;
-        }
-
-        var minionLi = (
+        return (
             <li key={minion.id} className={className} onClick={() => props.onMinionClick(minion)}>
                 <Minion imageProvider={props.imageProvider} minion={minion} />
             </li>
         );
-
-        return [firstPosition].concat(minionLi).concat(lastPosition);
     });
+
+    var positions = [];
+    if (props.showPositions) {
+        positions =
+            [<li key={"position-" + 0} className="position" onClick={() => props.onPositionClick(0)}></li>]
+            .concat(props.minions.map((m) => {
+                return <li key={"position-" + m.position + 1} className="position" onClick={() => props.onPositionClick(m.position + 1)}></li>;
+            }));
+    }
+
+    var lis = mix(positions, minions);
 
     return (
         <div className="side opponent">
-            <ul>{minions}</ul>
+            <ul>{lis}</ul>
         </div>
     );
 }

5f2acb862bf9409a56b83b595c4a0613af9f48f6
Tomas Ekholm <ekholm.tomas@gmail.com>
2016-05-25 17:08:53 +0200

    Added fan

--- src/hand.js
+++ src/hand.js
@@ -4,9 +4,11 @@ import ReactDOM from "react-dom";
 import Card from "./card/card";
 
 export default function Hand(props) {
-    const cards = props.cards.map(function (card) {
+    const numberOfCards = props.cards.length;
+    const startValue = - Math.floor(numberOfCards / 2);
+    const cards = props.cards.map(function (card, index) {
         return (
-            <li key={card.id} onClick={props.onCardClick.bind(null, card)}>
+            <li className={"position" + (startValue + index)} key={card.id} onClick={props.onCardClick.bind(null, card)}>
                 <Card selectedCard={props.selectedCard} imageProvider={props.imageProvider} card={card} />
             </li>
         );

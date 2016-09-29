7fd71c0b56564515606bb1986115a540629ecc43
Tomas Ekholm <ekholm.tomas@gmail.com>
2016-09-29 19:09:42 +0200

    Death sound works

--- tools/server.js
+++ tools/server.js
@@ -62,6 +62,15 @@ function getHearthheadSoundUrl(audioFileName) {
             return "http://" + data.attackSoundUrl;
         }
         return;
+    } else if (audioFileName.indexOf("Death.ogg") >= 0) {
+        var minionName = audioFileName.split("Death.ogg")[0].replace(/'/g, "&#039;");
+        var data = soundData.filter(function (data) {
+            return data.name === minionName;
+        })[0];
+        if (data) {
+            return "http://" + data.deathSoundUrl;
+        }
+        return;
     } else if (audioFileName.indexOf("Trigger.ogg") >= 0) {
         var minionName = audioFileName.split("Trigger.ogg")[0].replace(/'/g, "&#039;");
         var data = soundData.filter(function (data) {
